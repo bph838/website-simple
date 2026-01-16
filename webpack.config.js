cd220c7d98a71a8528592ddedab4051f6846d1dd
BP Hoops <benjaminpaulhooper@gmail.com>
2026-01-16 13:41:55 +0000

    have partials for footer and navigation

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 "use strict";
 "use strict";
 
+const fs = require("fs");
 const path = require("path");
+
 const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 const { SITE_TITLE } = require("./src/js/constants.js");
+const { loadEnvFile } = require("process");
 
 module.exports = (env, argv) => {
   const isProd = argv.mode === "production";
@@ -41,34 +45,79 @@ module.exports = (env, argv) => {
         ],
       }),
 
-      
       new HtmlWebpackPlugin({
         filename: "index.html",
-        template: "./src/template.ejs",
+        template: "./src/templates/main.html",
         chunks: ["index"], // only include index.js
         title: SITE_TITLE,
         templateParameters: {
           siteName: SITE_TITLE,
         },
+
+        navigation: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/navigation.html"),
+          "utf8"
+        ),
+        footer: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/footer.html"),
+          "utf8"
+        ),
       }),
       new HtmlWebpackPlugin({
         filename: "events.html",
-        template: "./src/template.ejs",
+        template: "./src/templates/main.html",
         chunks: ["events"], // only include events.js
-        title: "Events - " + SITE_TITLE,        
+        title: "Events - " + SITE_TITLE,
         templateParameters: {
           siteName: SITE_TITLE,
         },
+        navigation: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/navigation.html"),
+          "utf8"
+        ),
+        footer: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/footer.html"),
+          "utf8"
+        ),
       }),
       new HtmlWebpackPlugin({
         filename: "aboutus.html",
-        template: "./src/template.ejs",
+        template: "./src/templates/main.html",
         chunks: ["aboutus"], // only include about.js
-        title: "About - " + SITE_TITLE,        
+        title: "About - " + SITE_TITLE,
         templateParameters: {
           siteName: SITE_TITLE,
         },
+        navigation: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/navigation.html"),
+          "utf8"
+        ),
+        footer: fs.readFileSync(
+          path.resolve(__dirname, "src/partials/footer.html"),
+          "utf8"
+        ),
       }),
+
+      new HtmlWebpackPartialsPlugin({
+        path: path.join(__dirname, "./src/partials/navigation.html"),
+        location: "body",
+        template_filename: "*.html",
+      }),
+
+      /*new HtmlWebpackPartialsPlugin({
+        path: path.join(__dirname, "./src/partials"),
+        location: "body",
+        priority: "replace",
+        template_filename: "*.html",
+        options: {
+          navigation: {
+            template: "navigation.html",
+          },
+          footer: {
+            template: "footer.html",
+          },
+        },
+      }),*/
     ],
 
     optimization: {
